4c054c435037f2612f14568bbd3a20d29e0ae601
maohs <1543292509@qq.com>
2018-02-26 14:57:49 +0800

    开发登录页面

--- Mxd_community/app/src/main/java/com/project/mxd/mxd_community/LoginActivity.java
+++ Mxd_community/app/src/main/java/com/project/mxd/mxd_community/LoginActivity.java
@@ -1,8 +1,8 @@
 package com.project.mxd.mxd_community;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,9 +15,27 @@ public class LoginActivity extends AppCompatActivity {
         initViews();
     }
     private void initViews() {
-        LayoutInflater mainInflater = LayoutInflater.from(this);
-        View formView = mainInflater.inflate(R.layout.activity_login,null);
-//        TextView ignoreBtn =
 
+        TextView ignoreBtn =(TextView)findViewById(R.id.ignore_btn);
+        TextView loginDesc = (TextView)findViewById(R.id.loginDesc);
+        TextView loginBtn = (TextView)findViewById(R.id.login_btn);
+        //文本下划线
+        ignoreBtn.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+        loginDesc.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+        //忽略登录
+        ignoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        //登录/注册
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
