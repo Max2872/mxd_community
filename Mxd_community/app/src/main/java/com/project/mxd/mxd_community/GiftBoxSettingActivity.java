a9678bd7ad85348e4dafeec6c7477c0b20800b75
maohs <1543292509@qq.com>
2018-03-29 18:14:16 +0800

    开发商品详情

--- Mxd_community/app/src/main/java/com/project/mxd/mxd_community/GiftBoxSettingActivity.java
+++ Mxd_community/app/src/main/java/com/project/mxd/mxd_community/GiftBoxSettingActivity.java
@@ -1,10 +1,12 @@
 package com.project.mxd.mxd_community;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by maohs on 2018/3/27.
@@ -12,15 +14,35 @@ import android.widget.ImageView;
 
 public class GiftBoxSettingActivity extends AppCompatActivity {
     private ImageView backImage;
+    private TextView ensureBtn;
+    private TextView resetBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gift_box_setting);
         backImage = (ImageView)findViewById(R.id.top_bar_back);
+        ensureBtn = (TextView)findViewById(R.id.ensureBtn);
+        resetBtn = (TextView)findViewById(R.id.resetBtn);
         backImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        ensureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(GiftBoxSettingActivity.this, GiftBoxListActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
+                startActivity(intent);
+            }
+        });
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
--- Mxd_community/app/src/main/java/com/project/mxd/mxd_community/GoodsDetailActivity.java
+++ Mxd_community/app/src/main/java/com/project/mxd/mxd_community/GoodsDetailActivity.java
@@ -1,10 +1,12 @@
 package com.project.mxd.mxd_community;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by maohs on 2018/3/27.
@@ -12,16 +14,27 @@ import android.widget.ImageView;
 
 public class GoodsDetailActivity extends AppCompatActivity {
     private ImageView backImage;
-
+    private TextView buyBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.goods_detail);
         backImage = (ImageView)findViewById(R.id.top_bar_back);
+        buyBtn = (TextView) findViewById(R.id.buyBtn);
         backImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        buyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(GoodsDetailActivity.this, EditOrderActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
+                startActivity(intent);
             }
         });
     }
