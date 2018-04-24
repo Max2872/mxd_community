29d2e2d629d64f506527c176342d5c5304ffe21b
甘蓝 <1543292509@qq.com>
2018-04-24 22:39:20 +0800

    实现填写订单页面，以及接入网页

--- Mxd_community/app/src/main/java/com/project/mxd/mxd_community/CardFragment.java
+++ Mxd_community/app/src/main/java/com/project/mxd/mxd_community/CardFragment.java
@@ -6,17 +6,36 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by maohs on 2018/2/27.
  */
 
 public class CardFragment extends Fragment {
+    private WebView webView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.card_content,container,false);
+        webView = (WebView)view.findViewById(R.id.cardWeb);
+        //支持javascript
+        webView.getSettings().setJavaScriptEnabled(true);
+        //自适应屏幕
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
 
+                return false;
+            }
+        });
+        webView.loadUrl("http://www.baidu.com/");
         return view;
     }
+
 }
--- Mxd_community/app/src/main/java/com/project/mxd/mxd_community/EditOrderActivity.java
+++ Mxd_community/app/src/main/java/com/project/mxd/mxd_community/EditOrderActivity.java
@@ -1,10 +1,13 @@
 package com.project.mxd.mxd_community;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by maohs on 2018/3/29.
@@ -12,15 +15,54 @@ import android.widget.ImageView;
 
 public class EditOrderActivity extends AppCompatActivity {
     private ImageView backImage;
+    private RelativeLayout recieverInfo;
+
+    private ImageView orderNumDec;
+    private ImageView orderNumAdd;
+
+    private TextView orderSubmit;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_order);
         backImage = (ImageView)findViewById(R.id.top_bar_back);
+        recieverInfo = (RelativeLayout) findViewById(R.id.recieverInfo);
+        orderNumDec = (ImageView) findViewById(R.id.orderNumDec);
+        orderNumAdd = (ImageView) findViewById(R.id.orderNumAdd);
+
+        orderSubmit = (TextView) findViewById(R.id.orderSubmit);
+
         backImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        recieverInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        orderNumDec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        orderNumAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        orderSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
