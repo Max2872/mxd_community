43a94de4d1238ca9a70bf3ff8d67d9d36f70ce8f
甘蓝 <1543292509@qq.com>
2018-04-25 21:31:03 +0800

    update

--- Mxd_community/app/src/main/java/com/project/mxd/mxd_community/GiftBoxSettingActivity.java
+++ Mxd_community/app/src/main/java/com/project/mxd/mxd_community/GiftBoxSettingActivity.java
@@ -5,15 +5,31 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by maohs on 2018/3/27.
  */
 
-public class GiftBoxSettingActivity extends AppCompatActivity {
+public class GiftBoxSettingActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     private ImageView backImage;
+
+    private Spinner spin_who;
+    private Spinner spin_money;
+    private Spinner spin_use;
+    private Spinner spin_kind;
+
+    private BaseAdapter whoAdapter = null;
+    private BaseAdapter moneyAdapter = null;
+    private BaseAdapter useAdapter = null;
+    private BaseAdapter kindAdapter = null;
+
     private TextView ensureBtn;
     private TextView resetBtn;
     @Override
@@ -45,5 +61,92 @@ public class GiftBoxSettingActivity extends AppCompatActivity {
 
             }
         });
+        bindViews();
+    }
+    private void bindViews() {
+        spin_who = (Spinner)findViewById(R.id.spin_who);
+        spin_money = (Spinner)findViewById(R.id.spin_money);
+        spin_use = (Spinner)findViewById(R.id.spin_use);
+        spin_kind = (Spinner)findViewById(R.id.spin_kind);
+
+        ArrayList<GiftBoxSettingSpinnerItem> spin_who_Array = new ArrayList<>();
+        spin_who_Array.add(new GiftBoxSettingSpinnerItem("长辈"));
+        spin_who_Array.add(new GiftBoxSettingSpinnerItem("情侣"));
+        spin_who_Array.add(new GiftBoxSettingSpinnerItem("朋友"));
+        spin_who_Array.add(new GiftBoxSettingSpinnerItem("商业伙伴"));
+
+        ArrayList<ServiceMode> spin_money_Array = new ArrayList<>();
+        spin_money_Array.add(new ServiceMode("100元以内"));
+        spin_money_Array.add(new ServiceMode("100元~500元"));
+        spin_money_Array.add(new ServiceMode("500元~1000元"));
+        spin_money_Array.add(new ServiceMode("1000元以上"));
+
+        ArrayList<ServiceMode> spin_use_Array = new ArrayList<>();
+        spin_use_Array.add(new ServiceMode("生日"));
+        spin_use_Array.add(new ServiceMode("节日"));
+        spin_use_Array.add(new ServiceMode("升职"));
+        spin_use_Array.add(new ServiceMode("迁居"));
+
+        ArrayList<ServiceMode> spin_kind_Array = new ArrayList<>();
+        spin_kind_Array.add(new ServiceMode("数码"));
+        spin_kind_Array.add(new ServiceMode("养生"));
+        spin_kind_Array.add(new ServiceMode("服饰"));
+        spin_kind_Array.add(new ServiceMode("美食"));
+
+        whoAdapter = new ServiceAdapter<GiftBoxSettingSpinnerItem>(spin_who_Array,R.layout.order_service_spin) {
+            @Override
+            public void bindView(ViewHolder holder, GiftBoxSettingSpinnerItem obj) {
+                holder.setText(R.id.txt_name,obj.getContent());
+            }
+        };
+        moneyAdapter = new ServiceAdapter<ServiceMode>(spin_money_Array,R.layout.order_service_spin) {
+            @Override
+            public void bindView(ViewHolder holder, ServiceMode obj) {
+                holder.setText(R.id.txt_name,obj.getContent());
+            }
+        };
+
+        useAdapter = new ServiceAdapter<ServiceMode>(spin_use_Array,R.layout.order_service_spin) {
+            @Override
+            public void bindView(ViewHolder holder, ServiceMode obj) {
+                holder.setText(R.id.txt_name,obj.getContent());
+            }
+        };
+
+        kindAdapter = new ServiceAdapter<ServiceMode>(spin_kind_Array,R.layout.order_service_spin) {
+            @Override
+            public void bindView(ViewHolder holder, ServiceMode obj) {
+                holder.setText(R.id.txt_name,obj.getContent());
+            }
+        };
+        spin_who.setAdapter(whoAdapter);
+        spin_money.setAdapter(moneyAdapter);
+        spin_use.setAdapter(useAdapter);
+        spin_kind.setAdapter(kindAdapter);
+
+        spin_who.setOnItemSelectedListener(this);
+        spin_money.setOnItemSelectedListener(this);
+        spin_use.setOnItemSelectedListener(this);
+        spin_kind.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()) {
+            case R.id.spin_who:
+
+                break;
+            case R.id.spin_money:
+                break;
+            case R.id.spin_use:
+                break;
+            case R.id.spin_kind:
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
---
+++ Mxd_community/app/src/main/java/com/project/mxd/mxd_community/GiftBoxSettingSpinnerItem.java
@@ -0,0 +1,23 @@
+package com.project.mxd.mxd_community;
+
+/**
+ * Created by mao on 2018/4/25.
+ */
+
+public class GiftBoxSettingSpinnerItem {
+    private String content;
+    public GiftBoxSettingSpinnerItem() {
+
+    }
+    public GiftBoxSettingSpinnerItem(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
